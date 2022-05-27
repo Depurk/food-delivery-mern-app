3661e3f8b6175041755c8caa256369828a2b566c
Depurk <2depurk@gmail.com>
2022-05-27 14:57:01 +0530

    starting header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../img/logo.png";
 const Header = () => {
   return (
-    <div className='fixed z-50 bg-slate-300 w-screen p-6 px-16'>
-     {/* desktop & tablet */}
-    <div className='hidden md:flex w-full h-full bg-red-600 p-4'></div>
-    {/* mobile */}
-    <div className='flex md:hidden w-full h-full bg-blue-500 p-4'></div>
-    </div>
-    
-  )
-}
+    <header className="fixed z-50 w-screen p-6 px-16">
+      {/* desktop & tablet */}
+      <div className="hidden md:flex w-full h-full ">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </div>
+        <ul className="flex items-center gap-8 ml-auto ">
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Menu
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            About Us
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Service
+          </li>
+        </ul>
+      </div>
+      {/* mobile */}
+      <div className="flex md:hidden w-full h-full "></div>
+    </header>
+  );
+};
 
-export default Header
-
+export default Header;
