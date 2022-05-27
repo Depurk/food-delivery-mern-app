cc9fa4e08e1ad3e9787257f12014ae0422eed907
Depurk <2depurk@gmail.com>
2022-05-27 16:40:31 +0530

    header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,66 @@
 import React from "react";
 import Logo from "../img/logo.png";
+import Avatar from "../img/avatar.png";
+import { MdShoppingBasket } from "react-icons/md";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
-      {/* desktop & tablet */}
-      <div className="hidden md:flex w-full h-full ">
-        <div className="flex items-center gap-2">
-          <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold">City</p>
+      {/*for desktop & tablet */}
+
+      <div className="hidden md:flex w-full h-full items-center justify-between ">
+        {/* logo */}
+        <Link to={"/"}>
+          <motion.div
+            className="flex items-center gap-2 cursor-pointer"
+            whileTap={{ scale: 0.6 }}
+          >
+            <img src={Logo} className="w-8 object-cover" alt="logo" />
+            <p className="text-headingColor text-xl font-bold">City</p>
+          </motion.div>
+        </Link>
+
+        {/* menu */}
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8 ">
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Home
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Menu
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              About Us
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+              Service
+            </li>
+          </ul>
+
+          {/* cart */}
+          <motion.div
+            whileTap={{ scale: 0.6 }}
+            className="relative flex items-center justify-center"
+          >
+            <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNoBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>
+            </div>
+          </motion.div>
+
+          {/* user avatar */}
+
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={Avatar}
+            alt="user profile"
+            className="w-10 min-w-[40px] drop-shadow-xl cursor-pointer"
+          />
         </div>
-        <ul className="flex items-center gap-8 ml-auto ">
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-            Home
-          </li>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-            Menu
-          </li>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-            About Us
-          </li>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-            Service
-          </li>
-        </ul>
       </div>
-      {/* mobile */}
+      {/*for mobile */}
       <div className="flex md:hidden w-full h-full "></div>
     </header>
   );
