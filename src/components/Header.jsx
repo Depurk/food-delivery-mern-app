e3b38e7efbab13132b3053620d8988f0fa07b6c6
Depurk <2depurk@gmail.com>
2022-05-31 14:16:17 +0530

    added admin menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdShoppingBasket } from "react-icons/md";
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from "framer-motion";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
@@ -14,16 +14,19 @@ import { actionType } from "../context/reducer";
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const [{ user }, dispatch] =useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
   const login = async () => {
-    const {
-      user: { refreshToken, providerData },
-    } = await signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0],
-    });
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }
   };
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
@@ -74,11 +77,24 @@ const Header = () => {
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
-              src={Avatar}
+              src={user ? user.photoURL : Avatar}
               alt="user profile"
-              className="w-10 min-w-[40px] drop-shadow-xl cursor-pointer"
+              className="w-10 min-w-[40px] drop-shadow-xl cursor-pointer rounded-full"
               onClick={login}
             />
+            <div className="w-40 bg-gray-50 shodow-xl rounded-lg flex flex-col absolute top-12 right-0 ">
+              {user && user.uid === "113121088830895549817" && (
+                <Link to={"/createitem"}>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    New Item
+                    <MdAdd />
+                  </p>
+                </Link>
+              )}
+              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                Log Out <MdLogout />
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 import { StateProvider } from "./context/stateProvider";
 import { initialState } from "./context/initialState";
 import reducer from "./context/reducer";
