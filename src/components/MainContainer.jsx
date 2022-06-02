508ac43b42ba39a4c908f1a359f3f3a65f251562
Depurk <2depurk@gmail.com>
2022-06-02 22:51:23 +0530

    editing home container

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,7 @@ import Delivery from "../img/delivery.png";
 const MainContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div className="py-2 flex flex-col flex-1 items-start justify-center md:items-center">
+      <div className="py-2 flex flex-col flex-1 items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
@@ -17,6 +17,24 @@ const MainContainer = () => {
             />
           </div>
         </div>
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The fastest Delivery in
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem] ">
+            Your City{" "}
+          </span>
+        </p>
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
+          explicabo facilis repellendus quisquam, sit sint totam asperiores.
+          Cupiditate adipisci, cumque explicabo facilis officiis praesentium
+          impedit quo sed, illo, ipsa perferendis?
+        </p>
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
       </div>
       <div className="p-4 bg-blue-200 flex-1"></div>
     </div>
