92e2cffbe1b2a68de6ae5f8c2ef113302ca94986
Depurk <2depurk@gmail.com>
2022-06-12 08:53:06 +0530

    adding form to add item page

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { MdFastfood } from "react-icons/md";
 
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
@@ -15,7 +17,10 @@ const CreateContainer = () => {
     <div className="w-full min-h-screen flex items-center justify-center">
       <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
         {fields && (
-          <p
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
               alertStatus === "danger"
                 ? "bg-red-400 text-red-800"
@@ -23,8 +28,19 @@ const CreateContainer = () => {
             }`}
           >
             {msg}
-          </p>
+          </motion.p>
         )}
+        <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+          <MdFastfood className="text-xl text-gray-700" />
+          <input
+            type="text"
+            required
+            value={title}
+            placeholder="Give me a title..."
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
+          />
+        </div>
       </div>
     </div>
   );
